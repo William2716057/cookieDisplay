16adf838ae660e627f33d00741058f3316b7237c
William2716057 <77903649+William2716057@users.noreply.github.com>
2025-03-26 07:19:33 +1300

    typo fix

--- cookieDisplay.js
+++ cookieDisplay.js
@@ -1,7 +1,7 @@
 let cookies = document.cookie.split(';').reduce((acc, cookie) => {
-	let [name, value] = cookie.trim().split('=');
-	acc[name] value;
-	return acc;
-}, {};
+    let [name, value] = cookie.trim().split('=');
+    acc[name] = value;
+    return acc;
+}, {});
 
 console.log(cookies);
